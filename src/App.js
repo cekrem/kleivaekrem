fd56923f58cf149de5eb82dbaabc008f5ebbebbb
Christian Ekrem <cekrem@Christians-MacBook-Pro.local>
2018-09-19 05:27:33 +0200

    working prototype

--- src/App.js
+++ src/App.js
@@ -2,21 +2,41 @@ import React, { Component } from 'react'
 
 class App extends Component {
   state = {
-    index: 1,
+    index: 0,
+    backgrounds: [],
   }
 
-  getBackground = () => `${this.state.index}.jpeg`
-  handleClick = () => this.setState({
-    index: this.state.index > 17 ? 0 : this.state.index +1
-  })
+  handleClick = () => this.setState(state => ({
+    ...state,
+    index: Math.floor(Math.random() * state.backgrounds.length),
+  }))
+
+  createBackgroundArray = (length) => [...Array(17)].map(
+    (_, index) => `./${index+1}.jpeg`)
+    .sort(() => Math.random() - Math.random())
+
+  componentDidMount () {
+    const base = this.createBackgroundArray(17)
+
+    base.forEach((background, index) => {
+      setTimeout(() => {
+        this.setState({
+          backgrounds: this.state.backgrounds.concat(background),
+        })
+      }, index > 3 ? index * 1000 : 0)
+    })
+  }
 
   render () {
+    const {backgrounds, index} = this.state
     return (
-      <div onClick={this.handleClick} className="background" style={{backgroundImage: `url(${this.getBackground()})`}}>
+      <div className='wrapper' onClick={this.handleClick}>
         <div className="logo">
           Kleiva + Ekrem
         </div>
 
+        {backgrounds.map((url, i) => <div key={url} className={`background${index === i ? ' active' :''}`}
+                                     style={{backgroundImage: `url(${url})`}}/>)}
       </div>
     )
   }
